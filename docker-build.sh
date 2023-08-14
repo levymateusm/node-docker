d4f665f9721f14c92de93d5d9047368b4fcc6912
Levy Mateus Macedo <levy.macedo@ciandt.com>
2023-08-14 14:44:22 -0300

    add docker-build.sh

---
+++ docker-build.sh
@@ -0,0 +1,14 @@
+
+docker build --tag node-docker .
+docker run -it --rm -d -v mongodb:/data/db \
+  -v mongodb_config:/data/configdb -p 27017:27017 \
+  --network mongodb \
+  --name mongodb \
+  mongo
+docker run \
+  -it --rm -d \
+  --network mongodb \
+  --name rest-server \
+  -p 8000:8000 \
+  -e CONNECTIONSTRING=mongodb://mongodb:27017/notes \
+  node-docker
